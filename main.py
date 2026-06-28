8cdb2d8003ef50fe37f463a59b3da23a0dde58dd
TuhinDas661 <tuhindas01.official@gmail.com>
2026-06-28 04:36:09 -0700

    Data Transformation Stage Completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
+                                                                    
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -7,6 +9,19 @@ try:
     logger.info(f"{STAGE_NAME} started")
     data_ingestion_pipeline = DataIngestionTrainingPipeline()
     data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"{STAGE_NAME} completed")
+
+except Exception as e:
+    logger.error(f"{STAGE_NAME} failed with error: {e}")
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+    logger.info(f"{STAGE_NAME} started")
+    data_transformation_pipeline = DataTransformationTrainingPipeline()
+    data_transformation_pipeline.initiate_data_transformation()
     logger.info(f"{STAGE_NAME} completed")
 
 except Exception as e:
