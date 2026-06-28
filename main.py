2bd224707c054d62983b50993cf72fb6822bb533
TuhinDas661 <tuhindas01.official@gmail.com>
2026-06-28 13:54:52 -0700

    Model Trainer Stage Completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
                                                                     
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -27,3 +28,14 @@ try:
 except Exception as e:
     logger.error(f"{STAGE_NAME} failed with error: {e}")
     raise e
+
+STAGE_NAME = "Model Trainer Stage"
+
+try:
+    logger.info(f"{STAGE_NAME} started")
+    model_trainer_pipeline = ModelTrainerTrainingPipeline()
+    model_trainer_pipeline.initiate_model_trainer()
+    logger.info(f"{STAGE_NAME} completed")
+
+except Exception as e:
+    logger.error(f"{STAGE_NAME} failed with error: {e}")
